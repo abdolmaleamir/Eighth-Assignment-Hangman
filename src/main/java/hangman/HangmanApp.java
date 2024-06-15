231f0855a920d9e6fbfafb45d4dc3cdfeb01ec12
amirabdol <abdolmaleamir@gmail.com>
2024-06-15 20:31:30 +0330

    done with the start method

--- src/main/java/hangman/HangmanApp.java
+++ src/main/java/hangman/HangmanApp.java
@@ -10,6 +10,17 @@ import java.io.IOException;
 public class HangmanApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HangmanApp.class.getResource("menu-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 700, 400);
+        scene.getStylesheets().add(getClass().getResource("styles/hangman-menupage.css").toExternalForm());
+
+        stage.setTitle("Hangman");
+        stage.setScene(scene);
+        stage.show();
+
+
+
+
 
 
     }
