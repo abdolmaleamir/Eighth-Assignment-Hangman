a59e8396d8c7540d5e6a1a03f23e0754b62852f3
amirabdol <abdolmaleamir@gmail.com>
2024-06-15 20:28:28 +0330

    updated menucontroller and history class

--- src/main/java/hangman/HistoryGamesController.java
+++ src/main/java/hangman/HistoryGamesController.java
@@ -1,4 +1,116 @@
 package hangman;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.*;
+
 public class HistoryGamesController {
+    private User user;
+    private DatabaseManager dbManager;
+    @FXML
+    public Label titleLabel;
+    @FXML
+    public VBox historyContainer;
+
+    @FXML
+    public void initialize() {
+        dbManager = DatabaseManager.getDb();
+    }
+
+    public void createHistory(){
+        for(Game g: dbManager.getGamesOfUser(this.user.getUsername())){
+            HBox hBox = new HBox();
+            hBox.getStyleClass().add("gameContainer");
+            Label l1 = new Label("Game ID: " + g.getGameID());
+            Label l2 = new Label("Word : " + g.getWordToGuess());
+            Label l3 = new Label("Wrong Guesses : " + g.getWrongGuesses());
+            Label l4 = new Label("Time : " + g.getGameTime());
+            Label l5 = new Label("Win : " + (g.didWin() ? "Yes" : "No"));
+            VBox.setMargin(hBox, new Insets(10, 0, 0, 0));
+
+            hBox.getChildren().addAll(l1, l2, l3, l4 ,l5);
+
+            historyContainer.getChildren().add(hBox);
+        }
+    }
+
+    public void createLeaderboard(){
+        System.out.println("Creating Leaderboard");
+        HashMap<String, Integer> leaderboard = dbManager.getLeaderBoard();
+        leaderboard = (HashMap<String, Integer>) sortByValue(leaderboard);
+
+        leaderboard.forEach((username, wins) -> {
+            // System.out.println(username + ": " + wins);
+            HBox hBox = new HBox();
+            hBox.getStyleClass().add("gameContainer");
+            Label l1 = new Label("Username: " + username);
+            Label l2 = new Label("Wins : " + wins);
+            VBox.setMargin(hBox, new Insets(10, 0, 0, 0));
+
+            hBox.getChildren().addAll(l1, l2);
+
+            historyContainer.getChildren().add(hBox);
+        });
+    }
+
+    public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+        List<Map.Entry<K, V>> list = new ArrayList<>(map.entrySet());
+        list.sort(Collections.reverseOrder(Map.Entry.comparingByValue()));
+
+        Map<K, V> result = new LinkedHashMap<>();
+        for (Map.Entry<K, V> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+
+        return result;
+    }
+
+    public void backToMenu(ActionEvent actionEvent) {
+        // Load the Menu View
+        FXMLLoader fxmlLoader = new FXMLLoader(HangmanApp.class.getResource("menu-view.fxml"));
+        Parent root = null;
+        try {
+            root = fxmlLoader.load();
+        } catch (IOException e) {
+            System.out.println("Error While Getting Back to Menu: " + e.getMessage());
+        }
+
+        MenuController controller = fxmlLoader.getController();
+        controller.backToManu(this.user);
+
+        Scene scene = new Scene(root, 700, 400);
+        scene.getStylesheets().add(getClass().getResource("styles/hangman-menupage.css").toExternalForm());
+
+        Stage appStage = (Stage) (titleLabel).getScene().getWindow();
+        appStage.setScene(scene);
+        appStage.show();
+    }
+
+    public void setUser(User user){
+        this.user = user;
+        titleLabel.setText("History of Games Played By: " + user.getUsername());
+        createHistory();
+    }
+
+    public void initLeaderboard(User user){
+        this.user = user;
+        titleLabel.setText("Leaderboard");
+        createLeaderboard();
+    }
+
 }
+
+
+
+
+
--- src/main/java/hangman/MenuController.java
+++ src/main/java/hangman/MenuController.java
@@ -1,4 +1,228 @@
+
 package hangman;
 
+import javafx.animation.*;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class MenuController {
+    private DatabaseManager db;
+    private User currentUser;
+    // Menu Parts
+    @FXML
+    public VBox menuArea;
+    @FXML
+    public Label usernameLabel;
+    @FXML
+    public Label nameLabel;
+    // Login Parts
+    @FXML
+    public VBox loginArea;
+    @FXML
+    public HBox nameInputBox;
+    @FXML
+    public TextField usernameInput;
+    @FXML
+    public TextField nameInput;
+    @FXML
+    public PasswordField passwordInput;
+    @FXML
+    public Label toastText;
+    @FXML
+    public Button sendDataBtn;
+
+    @FXML
+    public void initialize(){
+        // Hide The Name Input Box To First Determine If The user Has been already registered or not.
+        nameInputBox.setVisible(false);
+        nameInputBox.setManaged(false);
+
+        // Get The Database
+        db = DatabaseManager.getDb();
+
+        // Init The Toast Message
+        toastText.setOpacity(0);
+
+        // Hide The Menu At first
+        menuArea.setVisible(false);
+        menuArea.setManaged(false);
+    }
+
+    public void backToManu(User user){
+        this.currentUser = user;
+        showToast("Back to Menu!", Color.WHITE);
+        switchToMenu();
+    }
+
+    public void showToast(String text, Color textColor){
+        toastText.setText(text);
+        toastText.setTextFill(textColor);
+
+        Timeline fadeInTimeline = new Timeline();
+        KeyFrame fadeInKey1 = new KeyFrame(Duration.millis(500), new KeyValue(toastText.opacityProperty(), 1));
+        fadeInTimeline.getKeyFrames().add(fadeInKey1);
+        fadeInTimeline.setOnFinished((ae) ->
+        {
+            new Thread(() -> {
+                try {
+                    Thread.sleep(3500);
+                }
+                catch (InterruptedException ignored) {}
+
+                Timeline fadeOutTimeline = new Timeline();
+                KeyFrame fadeOutKey1 = new KeyFrame(Duration.millis(500), new KeyValue (toastText.opacityProperty(), 0));
+                fadeOutTimeline.getKeyFrames().add(fadeOutKey1);
+                fadeOutTimeline.setOnFinished((aeb) -> toastText.setOpacity(0));
+                fadeOutTimeline.play();
+            }).start();
+        });
+        fadeInTimeline.play();
+    }
+    @FXML
+    public void searchUsername(ActionEvent actionEvent) {
+        doesUsernameExists(usernameInput.getText());
+    }
+
+    private boolean doesUsernameExists(String username){
+        if(db.getUser(username) == null ){
+            sendDataBtn.setText("Sign Up");
+            nameInputBox.setVisible(true);
+            nameInputBox.setManaged(true);
+            return false;
+        } else {
+            sendDataBtn.setText("Sign in");
+            nameInputBox.setVisible(false);
+            nameInputBox.setManaged(false);
+            return true;
+        }
+    }
+
+    @FXML
+    public void sendUserData(ActionEvent actionEvent) {
+        String username = usernameInput.getText();
+        String password = passwordInput.getText();
+        String name = nameInput.getText();
+
+        User user = db.getUser(username);
+
+        if (doesUsernameExists(username)) {
+            // Sign in
+            if (user.getPassword().equals(password)) {
+                currentUser = user;
+                showToast("Successfully Signed in! Welcome Back!", Color.WHITE);
+                switchToMenu();
+            } else {
+                showToast("Wrong Password!", Color.RED);
+            }
+        } else {
+            // Sign up
+            if (!name.isEmpty() && !password.isEmpty() && !username.isEmpty()) {
+
+                if (showConfirmationBox()) {
+                    // Register The New User.
+                    User newUser = new User(username, name, password);
+                    if (db.registerUser(name, username, password)) {
+                        currentUser = newUser;
+                        showToast("Successfully Signed up! Welcome!", Color.WHITE);
+                        switchToMenu();
+                    } else showToast("Failed to Sign up! Please Try again later.", Color.RED);
+
+                    // Doesn't work! (requestFocus)
+                } else nameInputBox.requestFocus();
+            } else showToast("Please fill the inputs.", Color.YELLOWGREEN);
+
+        }
+    }
+
+    private boolean showConfirmationBox() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation Dialog");
+        alert.setHeaderText("Confirmation Needed");
+        alert.setContentText("Are you sure you want to Create a new Account?");
+
+        // Block input to other windows
+        alert.initModality(Modality.APPLICATION_MODAL);
+
+        // Show the dialog and wait for the user to respond
+        AtomicBoolean result = new AtomicBoolean(false);
+        alert.showAndWait().ifPresent(response -> {
+            result.set(response == ButtonType.OK);
+        });
+
+        return result.getPlain();
+    }
+
+    private void switchToMenu(){
+        // Clean up Login/Register Page And Show the main menu.
+        loginArea.setVisible(false);
+        loginArea.setManaged(false);
+        menuArea.setVisible(true);
+        menuArea.setManaged(true);
+
+        // Show The Current User info.
+        usernameLabel.setText("Username: " + currentUser.getUsername());
+        nameLabel.setText("Name: " + currentUser.getName());
+    }
+
+    public void playGameHandler(ActionEvent actionEvent) throws IOException {
+        // Load the FXML file
+        FXMLLoader fxmlLoader = new FXMLLoader(HangmanApp.class.getResource("hangman-view.fxml"));
+        Parent root = fxmlLoader.load();
+
+        // Get the controller instance
+        HangmanController controller = fxmlLoader.getController();
+
+        // Pass the data to the controller
+        controller.setUser(this.currentUser);
+
+        // Create the scene with the new root node
+        Scene scene = new Scene(root, 700, 400);
+        scene.getStylesheets().add(getClass().getResource("styles/hangman-mainpage.css").toExternalForm());
+
+        // Get the current stage using the event source
+        Stage appStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        // Set the new scene on the stage
+        appStage.setScene(scene);
+        appStage.show();
+    }
+
+    public void showHistoryGames(ActionEvent actionEvent) throws IOException {
+        Button btn = (Button) actionEvent.getSource();
+        boolean isLeaderboard = btn.getText().equals("Leaderboard");
+
+        // Load the FXML file
+        FXMLLoader fxmlLoader = new FXMLLoader(HangmanApp.class.getResource("historyGames-view.fxml"));
+        Parent root = fxmlLoader.load();
+
+        // Get the controller instance
+        HistoryGamesController controller = fxmlLoader.getController();
+
+        // Pass the data to the controller
+        if (isLeaderboard) controller.initLeaderboard(this.currentUser);
+        else controller.setUser(this.currentUser);
+
+        // Create the scene with the new root node
+        Scene scene = new Scene(root, 900, 400);
+        scene.getStylesheets().add(getClass().getResource("styles/historyGames.css").toExternalForm());
+
+        // Get the current stage using the event source
+        Stage appStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        // Set the new scene on the stage
+        appStage.setScene(scene);
+        appStage.show();
+    }
 }
+
